09097c348435967a591165d2c97a52b2927a19bf
Dennis Tel <dennistel90@gmail.com>
2012-10-22 11:24:15 +0200

    socket fix

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -4,39 +4,39 @@ class Admin::PostsController < AdminController
   end
 
   def show
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
   end
 
   def new
-    @post = Post.new
+    # @post = Post.new
   end
 
   def create
-    @post = Post.new(params[:post])
+    # @post = Post.new(params[:post])
     if @post.save
-      redirect_to([:admin, @post], :flash => :success)
+      # redirect_to([:admin, @post], :flash => :success)
     else
-      render("new")
+      # render("new")
     end
   end
 
   def edit
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
     # raise @post.inspect
   end
 
   def update
     if @post.update_attributes(params[:post])
-      redirect_to([:admin, @post], :flash => :success)
+      # redirect_to([:admin, @post], :flash => :success)
     else
-      flash_now!(:error)
-      render("edit")
+      # flash_now!(:error)
+      # render("edit")
     end
   end
 
   def destroy
-    @post.destroy
-    redirect_to([:admin, :posts], :flash => :success)
+    # @post.destroy
+    # redirect_to([:admin, :posts], :flash => :success)
   end
 
 end
