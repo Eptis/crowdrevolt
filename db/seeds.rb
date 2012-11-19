9617939b247045b5e6cbe4e09cf27007245f4fe8
Dennis Tel <dennistel90@gmail.com>
2012-11-19 11:17:34 +0100

    standaard seed aangemaakt

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
 Admin.create(:username => "admin", :password => "123456", :password_confirmation => "123456") unless Admin.exists?
+User.create(:username => "front", :email => "dennistel90@gmail.com", :crypted_password => "$2a$10$jLbbWjKdG0UYOGyiXqjv8uVxoZBu5NM/mnWeim6qt9OvKsG5noHEK", :salt => "b7m6AwzLze7co2C79YT9", :activation_state => "active", :active => true) unless User.exists?
+
+
+
+
+Channel.create(:title => "Kanaal 1", :description => "Een proef kanaal.") unless Channel.exists?
+Episode.create(:title => "Aflevering 1", :description => "een proef aflevering", :channel_id => 1, :live => false, ) unless Episode.exists?
+
+
+
