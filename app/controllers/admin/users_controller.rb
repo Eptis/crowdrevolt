553a400b902f957abfb6577950dc92f227c0f7aa
Dennis Tel <dennistel90@gmail.com>
2012-12-06 11:45:58 +0100

    css fixes

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,10 @@ class Admin::UsersController < AdminController
   def index
     @users = User.all
   end
+  # def update
+  #   @user = User.find(params[:id])
+  #   if @user.update_attributes
+
+  #   end
+  # end
 end
