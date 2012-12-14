66972182a26d4beb0c3da5bbd7b84ed3e305be68
Erik de Groot <eriknv@hotmail.com>
2012-12-14 13:37:52 +0100

    migratie fix

--- db/migrate/20121214112944_join_fix.rb
+++ db/migrate/20121214112944_join_fix.rb
@@ -1,6 +1,6 @@
 class JoinFix < ActiveRecord::Migration
   def change
-    # drop_table :admins_channels
+    drop_table :admins_channels
 
     create_table :admins_channels, :id => false do |t|
       t.references :channel
